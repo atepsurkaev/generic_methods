34cdc6d1da98988c4ce25905161a338e9c34afdc
Tepsurkaev Abdurrahman <atepsurkaev2@student.tgm.ac.at>
2024-02-25 19:02:15 +0100

    Creation of the two classes + documentation comments

---
+++ src/atepsurkaev/GenericUtils.java
@@ -0,0 +1,10 @@
+package atepsurkaev;
+
+/**
+ * The GenericUtils class provides utility methods for manipulating arrays of generic types.
+ * @author Tepsurkaev Abdurrahman
+ * @version 25-02-2024
+ */
+
+public class GenericUtils {
+}
---
+++ src/atepsurkaev/Test.java
@@ -0,0 +1,11 @@
+package atepsurkaev;
+
+/**
+ * The Test class contains the main method to test the functionality of the GenericUtils class.
+ * @author Tepsurkaev Abdurrahman
+ * @version 25-02-2024
+ */
+
+public class Test {
+    public static void main(String[] args) {}
+}
