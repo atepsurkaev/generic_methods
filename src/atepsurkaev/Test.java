8416dd70f33db2e3d48ce45434afdb86823a9852
Tepsurkaev Abdurrahman <atepsurkaev2@student.tgm.ac.at>
2024-02-25 19:38:59 +0100

    Creation of the test class + comments

--- src/atepsurkaev/Test.java
+++ src/atepsurkaev/Test.java
@@ -7,5 +7,17 @@ package atepsurkaev;
  */
 
 public class Test {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        // Testing the reverse - method
+        System.out.println("\n--- Testing the reverse-method: ---");
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        System.out.println("Original array: " + java.util.Arrays.toString(intArray));
+        GenericUtils.reverse(intArray);
+        System.out.println("Reversed array: " + java.util.Arrays.toString(intArray));
+
+        // Testing the sum - method
+        System.out.println("\n--- Testing the sum-method: ---");
+        Number[] numbers = {1.2, 2.43, 3.5, 4, 5.33};
+        System.out.println("Sum of doubleArray: " + GenericUtils.sum(numbers));
+    }
 }
